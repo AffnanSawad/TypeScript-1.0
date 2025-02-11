8d7a21c03c492730860698d3316f52e52d30d4ac
Affnan Sawad <affnansawad2002@gmail.com>
2025-02-11 21:04:29 +0600

    Module -3 almost completed

--- Session1/session1/src/index.ts
+++ Session1/session1/src/index.ts
@@ -655,6 +655,337 @@ Result_2.showInformation();
 
 
 
+//  INHERITANCE : Ekta class er property , method onno Class e use kora.
+
+//  Main class take bole Parent class . r baki gulake child class bole.
+
+
+// Main / parent class
+
+class Parents_Info {
+
+ 
+    constructor( public name : string , public age : number) 
+
+    {    }
+     
+
+    Void_Display( working : boolean ){
+
+        console.log(`Parent name is ${this.name} who is ${this.age} and he is a working person which is ${working}`) ;
+    }
+
+
+}
+
+
+class Children  extends Parents_Info{
+
+    constructor(public name: string , public age : number)
+
+    {
+        
+        super(name , age) 
+
+
+    }
+
+   
+    void_children_show( grade : number ){
+      
+        console.log(`My  name is ${this.name} who is ${this.age} and he is in grade ${grade}`) ;
+
+    }
+
+}
+
+const X = new Children("Mohi" , 45 );
+X.Void_Display(true)
+
+
+const Y = new Children("Srabon" , 20 );
+Y.void_children_show(12)
+
+//  Output :
+// Parent name is Mohi who is 45 and he is a working person which is true
+// My  name is Srabon who is 20 and he is in grade 12
+
+
+//  Type Guard : 1 . TypeOf , 2 . in guard
+
+// Type of : check kore variable type
+//  in guard : check kore variable / property exists kore kina.
+
+//   Type Guard 
+
+type dataType = string | number ;
+
+
+const summ = ( a1 : dataType , a2 : dataType) : dataType =>{
+
+ if( typeof a1 ==='number' && typeof a2==='number' ){
+
+    return a1 + a2 ;
+ }
+ else{
+    return a1.toString() + a2.toString();
+ }
+
+
+}
+
+const result1 = summ(2,3);
+const result__12 = summ("2","3");
+
+console.log(result1)
+console.log(result__12);
+
+
+//  in guard 
+
+type Normaluser = {
+  
+    name: string;
+
+}
+type Adminuser = {
+  
+    name: string;
+
+    role: 'Admin' ;
+
+}
+
+
+const loggedInUser = ( user : Normaluser | Adminuser) =>{
+ 
+    if('role' in user){
+
+        console.log(` Log in user ${user.name} who is ${user.role}  `)
+    }
+
+    else{
+        console.log(` Log in user ${user.name}   `)
+    }
+
+
+}
+
+
+
+//  Type Guard : instance of = it is used to use Class properties , method  to use in function which is outside the class.
+
+    // Type guard using instance of
+
+    class Animal{
+   
+        constructor( public name:string , public age: number , public sound:string) {}
+
+         makeSound(){
+           
+            console.log(`The ${this.name} who is ${this.age} years old , says ${this.sound}`)
+         
+        }
+   
+
+    }
+
+
+    class Dog extends Animal {
+   
+        constructor( public name:string , public age: number , public sound:string) {   
+            // 
+            
+            super(name,age,sound);
+
+             
+        }
+
+        makeBark(){
+            console.log(`bark bark`)
+        }
+
+   }
+
+
+
+
+   class Cat extends Animal {
+   
+    constructor( public name:string , public age: number , public sound:string) {   
+        // 
+        
+        super(name,age,sound);
+
+         
+    }
+
+    makeMeow(){
+        console.log(`meow`)
+    }
+
+}
+
+
+
+const cat1 = new Animal('Persian', 12 , 'meow');
+const dog1 = new Animal('German', 12 , 'gheow');
+
+// 
+const isDog = ( animal : Animal) : animal is Dog => {
+
+    return animal instanceof Dog ;
+}
+
+const isCat = ( animal : Animal) : animal is Cat => {
+
+    return animal instanceof Cat ;
+}
+
+const soundHobe = ( animal : Animal ) => {
+
+    if(isDog(animal)){
+
+        animal.makeBark()
+    }
+    else if( isCat(animal)){
+        animal.makeMeow()
+    }
+    else{
+        animal.makeSound();
+    }
+}
+
+//  Access modifier : private variable ke function er maddhe use kora. 
+
+    //  Access Modifier :
+
+    class BankAccount{
+
+    
+        constructor( public readonly id: number , private _balance: number ){}
+
+        addBalance( amount : number ){
+
+            this._balance = this._balance + amount ;
+        }
+
+        newBalance(){
+          return  this._balance;
+        }
+
+
+    }
+
+    const First_Balance = new BankAccount(221, 63000);
+    
+    // First_Balance.balacne
+    // new balance add hocce na
+
+    First_Balance.addBalance(3000);
+    
+    const my_new_total_Balance  = First_Balance.newBalance();
+
+    console.log(my_new_total_Balance);
+
+
+
+
+    //  Getter / Setter : functin call na kore Private variable e access kora.
+
+    //   GETTER & SETTER
+
+class BankAccount1{
+
+    
+    constructor( public readonly id: number , private _balance: number ){}
+
+  
+    set deposit1( amount : number ){
+
+        this._balance = this._balance + amount ;
+    }
+
+    get newAmount1(){
+
+        return this._balance ;
+    }
+
+
+}
+
+const First_Balance1 = new BankAccount1(221, 63000);
+
+//  add new amount of money . without calling function
+First_Balance1.deposit1 = 2000;
+
+const NewBalance1 = First_Balance1.newAmount1
+
+console.log(NewBalance1);
+
+
+
+
+  //  Static : memory location same thake. 
+
+  class Count {
+
+    static balance : number = 0 ;
+
+    static incrimant(){
+    
+        return ( Count.balance = Count.balance + 1  )
+
+    }
+
+    static decrimant(){
+    
+        return ( Count.balance= Count.balance - 1  )
+    }
+}
+
+// ANSWER: 1,2,3
+console.log( Count.incrimant())   
+console.log( Count.incrimant())
+console.log( Count.incrimant())
+console.log( Count.incrimant())
+
+//   ANSWER : -1,-2,-3
+console.log( Count.decrimant())   
+console.log( Count.decrimant())
+console.log( Count.decrimant())
+console.log( Count.decrimant())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
